95f40c4b7c0eac1fadbfd5203afe43e8460de5d2
vincent <v1ncentzhuang>
2023-10-17 14:57:36 -0400

    binary search rough tests based off test_graph_family() method;

--- search/test.cpp
+++ search/test.cpp
@@ -32,8 +32,32 @@ void test_hyper_graph_family() {
     assert(!f4.sample(edge_count, 0.93).is_core_empty());
 }
 
+void test_binary_search_for_alpha() {
+    const uint32_t edge_count = 100000;
+    float epsilon = 1e-6;
+
+    HyperGraphFamily f1({3}, {1});
+    assert(f1.binary_search(epsilon) > 0.81);
+    assert(f1.binary_search(epsilon) < 0.83);
+
+
+    HyperGraphFamily f2({4}, {1});
+    assert(f2.binary_search(epsilon) > 0.76);
+    assert(f2.binary_search(epsilon) < 0.78);
+
+
+    HyperGraphFamily f3({5}, {1});
+    assert(f3.binary_search(epsilon) > 0.69);
+    assert(f3.binary_search(epsilon) < 0.71);
+
+    HyperGraphFamily f4({3, 21}, {89, 11});
+    assert(f4.binary_search(epsilon) > 0.91);
+    assert(f4.binary_search(epsilon) < 0.93);
+}
+
 int main() {
     test_hyper_graph();
     test_hyper_graph_family();
+    test_binary_search_for_alpha();
     std::cout << "OK" << std::endl;
 }
